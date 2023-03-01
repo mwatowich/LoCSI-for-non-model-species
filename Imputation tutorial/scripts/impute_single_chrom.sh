3cc123a53e82036f13ff84bd6eda8bdb795acc46
Marina Watowich <49698282+mwatowich@users.noreply.github.com>
2023-03-01 12:01:00 -0700

    Update impute_single_chrom.sh

--- Imputation tutorial/scripts/impute_single_chrom.sh
+++ Imputation tutorial/scripts/impute_single_chrom.sh
@@ -2,7 +2,6 @@
 ## low coverage imputation using loimpute
 
 sample=`sed -n ${SLURM_ARRAY_TASK_ID}p samples`
-coverage=${1}
 chrom=${2}
 
 module load speedseq/0.1.2
@@ -11,11 +10,11 @@ module load singularity/3.6.3
 module load bcftools/1.10.2
 module load tabix/0.2.6
 
-singularity exec -B /scratch/mwatowic/impute loimpute_latest.sif loimpute \
+singularity exec -B /path loimpute_latest.sif loimpute \
     -ne 1000 \
     -id ${sample} \
-    -i pileups/${sample}.${coverage}.${chrom}.gz \
+    -i pileups/${sample}.${chrom}.gz \
     -h mgap_new/mgap.chr${chrom}.phased.AFfilt.vcf.gz \
-    -o imputed/${sample}.${coverage}.${chrom}
+    -o imputed/${sample}.${chrom}
 
-tabix -p vcf imputed/${sample}.${coverage}.${chrom}.vcf.gz
+tabix -p vcf imputed/${sample}.${chrom}.vcf.gz
