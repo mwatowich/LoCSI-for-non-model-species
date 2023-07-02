5f5a278691022d533e2cc0ad81b37d26d7e58bd7
Marina Watowich <49698282+mwatowich@users.noreply.github.com>
2023-07-02 14:52:13 -0500

    Update per_sample_missingness.sh

--- Imputation tutorial/scripts/per_sample_missingness.sh
+++ Imputation tutorial/scripts/per_sample_missingness.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 module load bcftools/1.12.0
-
-bcftools stats -s - --threads 24 reference.chr${SLURM_ARRAY_TASK_ID}.vcf.gz > chr${SLURM_ARRAY_TASK_ID}.stats
+bcftools stats -S samples --threads 24 reference.chr1.vcf.gz > chr1.stats
